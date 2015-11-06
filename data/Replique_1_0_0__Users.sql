8c0de3fa8645ad4e8c65adda92c5ecee98aaeac2
Yoanes Koesno <yoanes.koesno@touchcorp.com>
2015-11-06 18:25:35 +1100

    data for user

---
+++ data/Replique_1_0_0__Users.sql
@@ -0,0 +1,22 @@
+-- 
+use replique;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `salt` varchar(50) NOT NULL,
+  `key` varchar(50) NOT NULL DEFAULT '',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `inactives` (
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(10) NOT NULL,
+  `token` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+  FOREIGN_KEY (`user_id`) REFERENCES users(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
