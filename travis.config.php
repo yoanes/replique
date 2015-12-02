5e55091ec2272642ec6c55fb72aa6c8a8dc66952
Yoanes Koesno <y.koesno@gmail.com>
2015-12-02 14:25:16 +1100

    additional config override for travis

---
+++ travis.config.php
@@ -0,0 +1,31 @@
+]['Datasources'] => [
+        'default' => [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
+            'persistent' => false,
+            'host' => 'localhost',
+            'username' => 'root',
+            'password' => 'password',
+            'database' => 'replique',
+            'encoding' => 'utf8',
+            'timezone' => 'UTC',
+            'cacheMetadata' => true,
+            'log' => true,
+            'quoteIdentifiers' => false,
+        ],
+
+        'test' => [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
+            'persistent' => false,
+            'host' => 'localhost',
+            'username' => 'root',
+            'password' => 'password',
+            'database' => 'replique_test',
+            'encoding' => 'utf8',
+            'timezone' => 'UTC',
+            'cacheMetadata' => true,
+            'quoteIdentifiers' => false,
+            'log' => false,
+        ],
+];
