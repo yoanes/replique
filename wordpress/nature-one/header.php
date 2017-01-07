aa9ea58a8ea95c7cf88ca613093e664f6306cb5b
Yoanes Koesno <y.koesno@gmail.com>
2017-01-07 22:28:14 +1100

    add analytics code

--- wordpress/nature-one/header.php
+++ wordpress/nature-one/header.php
@@ -17,7 +17,16 @@
 </head>
 
 <body <?php body_class(); ?>>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-6828636-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <?php if ( (of_get_option('innerpageslider', true) != 'hide') || is_home() || is_front_page() ) { ?>
     <div class="slider-wrapper">
            <?php
